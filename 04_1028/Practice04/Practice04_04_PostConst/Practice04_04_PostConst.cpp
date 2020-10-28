7bc04029f5339014a54669fc9c15f757ee7da89f
TastuyaTsujimoto <machikado111@gmail.com>
2020-10-28 17:57:14 +0900

    第４回実習その４
    後置コンスト（＝コンストメンバ関数）
    　書き方・使い方
    　どのような効果があるか
    　どういう関数に使えばいいか

---
+++ 04_1028/Practice04/Practice04_04_PostConst/Practice04_04_PostConst.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+
+class Test
+{
+public:
+	void SetValue(int value);
+	int GetValue()const;
+private:
+	int Value;
+
+};
+
+void Test::SetValue(int value)
+{
+	Value = value;
+}
+
+int Test::GetValue()const
+{
+	return Value;
+}
+
+int main()
+{
+	Test test;
+	test.SetValue(1000);
+	int ans = test.GetValue();
+
+	return 0;
+}
+
