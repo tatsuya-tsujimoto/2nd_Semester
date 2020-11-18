43f2c222e8e97d3ff9eb7bb5c2687276b4e15c65
TastuyaTsujimoto <machikado111@gmail.com>
2020-11-18 17:19:09 +0900

    第６回実習その４
    Operatorの基本的な作り方、考え方について

---
+++ 06_1118/Practice06/Practice06_04_Operator/Practice06_04_Operator.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+
+
+// 書き方の例としてシンプルなTestクラスを作る
+
+class Test
+{
+public:
+	Test(int val):m_Val(val)
+	{}
+
+
+	// Test と int を足し算するための + オペレーター
+	int operator +(int val)
+	{
+		return (m_Val + val);
+	}
+
+
+private:
+	int m_Val;
+
+
+
+};
+
+
+int main()
+{
+	Test t(10);
+	int ans = t + 100 + 111;		// Testが持っているm_Valと足し算ができるようにしたい
+	printf("ans = %d\n", ans);
+
+
+
+	return 0;
+}
