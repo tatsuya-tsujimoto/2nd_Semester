3ff42bc339541ec4e53dff5e754c07edcaf46a17
TastuyaTsujimoto <machikado111@gmail.com>
2020-11-18 18:04:18 +0900

    no message

--- 06_1118/Practice06/Practice06_05_Operator_Equal/Character.h
+++ 06_1118/Practice06/Practice06_05_Operator_Equal/Character.h
@@ -1,4 +1,3 @@
-
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
--- 06_1118/Practice06/Practice06_05_Operator_Equal/Practice06_05_Operator_Equal.cpp
+++ 06_1118/Practice06/Practice06_05_Operator_Equal/Practice06_05_Operator_Equal.cpp
@@ -5,7 +5,8 @@ int main()
 {
 
 	Character ch1;
-	Character ch2 = ch1;
+	Character ch2 = ch1;	// 実体を作る際に合わせて初期化する　　コピーコンストラクタが呼び出される
+	ch2 = ch1;				// 代入なので代入演算子のオーバーロードが呼び出される
 
 
 
